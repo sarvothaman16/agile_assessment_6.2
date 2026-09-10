1676129d51e92f67bab7ce78990f74aee196b0e2
Sarvothaman R J <sarvothamanmsd@gmail.com>
2026-09-10 20:28:39 +0530

    Create InvalidVehicleException.java

---
+++ src/main/java/com/traffic/exceptions/InvalidVehicleException.java
@@ -0,0 +1,8 @@
+package com.traffic.exceptions;
+
+public class InvalidVehicleException extends Exception {
+
+    public InvalidVehicleException(String message) {
+        super(message);
+    }
+}
